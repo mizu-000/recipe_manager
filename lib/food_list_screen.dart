3ea1a6f0055d26cb87da2eae077bad0fbdfb7d90
Mizuki Ichikawa <mizuki.ichikawa.9k@stu.hosei.ac.jp>
2025-01-14 11:53:17 +0900

    リスト削除機能追加
    リスト削除機能追加

--- lib/food_list_screen.dart
+++ lib/food_list_screen.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:sqflite/sqflite.dart';
 
-class FoodListScreen extends StatelessWidget {
+class FoodListScreen extends StatefulWidget { // StatefulWidgetに変更
   final Database database;
 
   const FoodListScreen({Key? key, required this.database}) : super(key: key);
 
+  @override
+  State<FoodListScreen> createState() => _FoodListScreenState();
+}
+
+class _FoodListScreenState extends State<FoodListScreen> {
   // データベースから食材データを取得する関数
   Future<List<Map<String, dynamic>>> getFoods() async {
-    final db = database;
+    final db = widget.database;
     final List<Map<String, dynamic>> maps = await db.query('foods');
     return maps;
   }
@@ -28,10 +33,34 @@ class FoodListScreen extends StatelessWidget {
               itemCount: foods.length,
               itemBuilder: (context, index) {
                 final food = foods[index];
-                return ListTile(
-                  title: Text(food['name']),
-                  subtitle: Text(
-                      '数量: ${food['quantity']} ${food['unit']}, 消費期限: ${food['expiryDate']}'),
+                return Dismissible( // DismissibleでListTileをラップ
+                  key: UniqueKey(), // 各アイテムに一意のキーを設定
+                  onDismissed: (direction) async {
+                    // スワイプして削除されたときの処理
+                    final db = widget.database;
+                    await db.delete(
+                      'foods',
+                      where: 'id = ?',
+                      whereArgs: [food['id']],
+                    );
+                    // リストを更新するためにsetStateを呼び出す
+                    setState(() {});
+                  },
+                  background: Container(
+                    color: Colors.red, // スワイプ時の背景色
+                    child: const Align(
+                      alignment: Alignment.centerRight,
+                      child: Padding(
+                        padding: EdgeInsets.only(right: 16.0),
+                        child: Icon(Icons.delete, color: Colors.white),
+                      ),
+                    ),
+                  ),
+                  child: ListTile(
+                    title: Text(food['name']),
+                    subtitle: Text(
+                        '数量: ${food['quantity']} ${food['unit']}, 消費期限: ${food['expiryDate']}'),
+                  ),
                 );
               },
             );
